837fb57aa6e1c0caac4235745b2f32da4440af30
Pavani Rajula <rpavani1998@gmail.com>
2019-02-21 23:32:47 +0530

    Updated registration form

--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -3,40 +3,62 @@ import { Student } from '../student';
 import { StudentService } from '../student.service';
 import {FormGroup, FormArray, FormBuilder} from '@angular/forms';
 import { Location } from '@angular/common';
+import { EducationDetails } from '../education-details';
 
 @Component({
-  selector: 'app-add-student',
+  selector: 'app-registartion',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 
 export class RegisterStudentComponent{
-
   student = new Student();
   submitted = false;
-  form: FormGroup;
+  myForm: FormGroup;
   backlogs = ["Never had any backlog", "Cleared All", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "More than 10"];
+  data = {
+    education_details : [
+      {
+      certificate_degree_name: '',
+      major: '',
+      institute_university_name : '',
+      start_date: null,
+      completion_date: null,
+      percentage: 100,
+      cgpa: 10,
+      proof_document: null    
+      }
+    ],
+    experience_details : [ 
+      {
+      is_current_job: false,
+      start_date: null,
+      end_date: null,
+      job_title: '',
+      company_name: '',
+      job_location_city:  '',
+      description: '',
+      proof_document: null
+      }
+    ]
+  }
   constructor(
     private studentService: StudentService,
     private location: Location,
     private fb: FormBuilder
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.form = this.fb.group({
-      items: this.fb.array([this.createItem()])
+    this.myForm = this.fb.group({
+      education_details: this.fb.array([]),
+      experience_details: this.fb.array([])
     })
+    this.setEducationDetails();
   }
 
-  createItem() {
-    return this.fb.group({
-      name: ['Jon'],
-      surname: ['Doe']
-    })
-  }
-
-  addNext() {
-    (this.form.controls['items'] as FormArray).push(this.createItem())
+  deleteEducationDetails(index) {
+    let control = <FormArray>this.myForm.controls.education_details;
+    control.removeAt(index)
   }
 
   newStudent(): void {
@@ -49,14 +71,92 @@ export class RegisterStudentComponent{
    this.save();
  }
 
+ setExperienceDetails() {
+  let control = <FormArray>this.myForm.controls.education_details;
+  this.data.experience_details.forEach(x => {
+    control.push(this.fb.group({ 
+      is_current_job: x.is_current_job,
+      start_date: x.start_date,
+      end_date: x.end_date,
+      job_title: x.job_title,
+      company_name: x.company_name,
+      job_location_city: x.job_location_city,
+      description: x.description,
+      proof_document: x.proof_document
+     }))
+  })
+}
+
+addNewExperienceForm() {
+  let control = <FormArray>this.myForm.controls.experience_details;
+  control.push(
+    this.fb.group({
+      is_current_job: false,
+      start_date: null,
+      end_date: null,
+      job_title: '',
+      company_name: '',
+      job_location_city:  '',
+      description: '',
+      proof_document: null
+    })
+  )
+}
+
+deleteExperienceDetails(index) {
+  let control = <FormArray>this.myForm.controls.experience_details;
+  control.removeAt(index)
+}
+
+setEducationDetails() {
+  let control = <FormArray>this.myForm.controls.education_details;
+  this.data.education_details.forEach(x => {
+    control.push(this.fb.group({ 
+      certificate_degree_name: x.certificate_degree_name,
+      major: x.major,
+      institute_university_name : x.institute_university_name,
+      start_date: x.start_date,
+      completion_date: x.completion_date,
+      percentage: x.percentage,
+      cgpa: x.cgpa,
+      proof_document: x.proof_document
+     }))
+  })
+}
+
+addNewEducationForm() {
+  let control = <FormArray>this.myForm.controls.education_details;
+  control.push(
+    this.fb.group({
+      certificate_degree_name: '',
+      major: '',
+      institute_university_name : '',
+      start_date: null,
+      completion_date: null,
+      percentage: 100,
+      cgpa: 10,
+      proof_document: null
+    })
+  )
+}
+
   goBack(): void {
     this.location.back();
   }
 
   private save(): void {
     console.info("student info", this.student);
-    this.studentService.addStudent(this.student)
-        .subscribe();
+    this.student.educational_details = this.myForm.value.education_details;
+    this.student.experience_details = this.myForm.value.experience_details;
+    this.studentService.addStudentProfile(this.student)
+    .subscribe();
+    for(let i in this.student.educational_details){
+       this.studentService.addStudentEducationDetails(this.student.educational_details[i]).subscribe(); 
+    }
+    for(let i in this.student.experience_details){
+      this.studentService.addStudentExperienceDetails(this.student.experience_details[i]).subscribe();  
+   }
   }
+
 }
 
