7853d08840ddce299552beeb0721f95cf2f713b7
Pavani Rajula <rpavani1998@gmail.com>
2019-05-31 00:40:57 +0530

    Project Feature and Few Bug Fix

---
+++ client/src/app/models/project.ts
@@ -0,0 +1,9 @@
+export class Project {
+    id: string
+    roll_no : string
+    description: string
+    title: string
+    start_date: Date = null
+    end_date: Date =  null
+    url: string
+  }
