b6365654be71c57f6a935ba3b779501bb8fb67ac
Pavani Rajula <rpavani1998@gmail.com>
2019-05-31 00:38:52 +0530

    Generate Latex PDF

---
+++ server/route/project.route.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+    const project = require('../controllers/project.controller.js');
+
+
+    app.post('/api/project', project.create);
+ 
+    app.get('/api/project', project.findAll);
+ 
+    app.get('/api/projects/:roll_no', project.findAll);
+ 
+    app.put('/api/project', project.update);
+
+    app.delete('/api/project/:userId', project.delete);
+}
